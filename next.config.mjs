75fab1979cfaa9615b573d1afbe3ac92224e3c77
Sadriddin <sadriddinaxhmadullayev@gmail.com>
2026-03-05 13:30:21 +0500

    Updating to nev server

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'https://omonatserver.vercel.app/api/:path*',
+				destination: 'http://13.60.93.106:2026/api/:path*',
 			},
 		]
 	},
